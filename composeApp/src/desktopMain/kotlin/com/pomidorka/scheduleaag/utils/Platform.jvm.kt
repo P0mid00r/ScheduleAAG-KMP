40e3c224d7d6074fd8f810a90d6b9c8689620713
P0mid00r <sergebelousov2018@gmail.com>
2025-12-06 00:11:41 +0700

    Хранение текущей десктоп платформы

--- composeApp/src/desktopMain/kotlin/com/pomidorka/scheduleaag/utils/Platform.jvm.kt
+++ composeApp/src/desktopMain/kotlin/com/pomidorka/scheduleaag/utils/Platform.jvm.kt
@@ -1,8 +1,11 @@
 package com.pomidorka.scheduleaag.utils
 
+import org.jetbrains.skiko.hostOs
+
 class JVMPlatform : Platform {
     override val type = PlatformType.Desktop
-    override val name: String = "Java ${System.getProperty("java.version")}"
+    override val name: String = hostOs.id
+//    override val name: String = "Java ${System.getProperty("java.version")}"
 }
 
 actual fun currentPlatform(): Platform = JVMPlatform()
