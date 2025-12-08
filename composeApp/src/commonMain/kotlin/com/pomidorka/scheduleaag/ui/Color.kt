22ff22329fdda9950d4fc6ddb88b7bc8ae5837e7
P0mid00r <sergebelousov2018@gmail.com>
2025-12-09 00:01:54 +0700

    Добавлен перевод цвета в его hex код

--- composeApp/src/commonMain/kotlin/com/pomidorka/scheduleaag/ui/Color.kt
+++ composeApp/src/commonMain/kotlin/com/pomidorka/scheduleaag/ui/Color.kt
@@ -4,3 +4,14 @@ import androidx.compose.ui.graphics.Color
 
 val Green = Color(0, 146, 71)
 val Brown = Color(191, 159, 98)
+
+fun Color.toHexString(): String {
+    val hex = value.toString(16)
+        .padStart(8, '0')
+        .uppercase()
+        .apply {
+            removeRange(8, length)
+        }
+        .substring(2)
+    return "#$hex"
+}
